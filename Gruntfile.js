a74d11dd55c35dccecf78a5d0ee8f0a224e9ee01
vitalii.mishchenko <vitalii.mishchenko@globallogic.com>
2015-07-08 19:28:00 +0300

    init

---
+++ Gruntfile.js
@@ -0,0 +1,193 @@
+module.exports = function (grunt) {
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-html-build');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-bump');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
+
+    var userConfig = require('./build.config.js');
+
+    var taskConfig = {
+        pkg: grunt.file.readJSON("package.json"),
+        copy: {
+            // DEVELOPMENT
+            app_assets: {
+                files: [
+                    {
+                        src: ['**'],
+                        dest: '<%= build_dir %>/src/assets',
+                        cwd: 'src/assets',
+                        expand: true,
+                        flatten: false
+                    }
+                ]
+            },
+            app_js: {
+                files: [
+                    {
+                        src: ['**'],
+                        dest: '<%= build_dir %>/src/js',
+                        cwd: 'src/js',
+                        expand: true,
+                        flatten: false
+                    }
+                ]
+            },
+
+            //vendor
+            vendor_js: {
+                files: [
+                    {
+                        src: ['<%= vendor_files.js %>'],
+                        dest: '<%= build_dir %>',
+                        cwd: '.',
+                        expand: true
+                    }
+                ]
+            },
+            vendor_css: {
+                files: [
+                    {
+                        src: ['<%= vendor_files.css %>'],
+                        dest: '<%= build_dir %>/src/assets/css/vendor',
+                        cwd: '.',
+                        expand: true,
+                        flatten: true
+                    }
+                ]
+            },
+            vendor_fonts: {
+                files: [
+                    {
+                        src: ['<%= vendor_files.fonts %>'],
+                        dest: '<%= build_dir %>/assets/fonts',
+                        cwd: '.',
+                        expand: true,
+                        flatten: true
+                    }
+                ]
+            },
+
+            // COMPILE
+            app_assets_compile: {
+                files: [
+                    {
+                        src: ['**'],
+                        dest: '<%= compile_dir %>/src/assets',
+                        cwd: 'src/assets',
+                        expand: true,
+                        flatten: false
+                    }
+                ]
+            },
+            vendor_js_compile: {
+                files: [
+                    {
+                        src: ['<%= compile_files.js %>'],
+                        dest: '<%= compile_dir %>',
+                        cwd: '.',
+                        expand: true
+                    }
+                ]
+            },
+        },
+        less: {
+            dev: {
+                options: {
+                    paths: ["assets/css"]
+                },
+                files: {
+                    "<%= build_dir %>/src/assets/css/default-<%= pkg.name %>-<%= pkg.version %>.css": '<%= app_files.less.default %>'
+                }
+            },
+            compile: {
+                options: {
+                    paths: ["assets/css"]
+                },
+                files: {
+                    "<%= compile_dir %>/src/assets/css/default-<%= pkg.name %>-<%= pkg.version %>.css": '<%= app_files.less.default %>'
+                }
+            }
+        },
+        clean: {
+            build: [
+                '<%= build_dir %>'
+            ],
+            assets_build: [
+                '<%= build_dir %>/assets'
+            ],
+            compile: [
+                '<%= compile_dir %>'
+            ]
+        },
+        watch: {
+            app_js: {
+                files: [
+                    '<%= app_files.js %>',
+                    '<%= app_files.templates %>'
+                ],
+                tasks: ['copy:app_js']
+            },
+            assets: {
+                files: ['src/assets/**'],
+                tasks: ['clean:assets_build', 'copy:app_assets', 'copy:vendor_css', 'copy:vendor_fonts', 'less:dev']
+            },
+            less: {
+                files: ['src/**/*.less'],
+                tasks: ['less:dev']
+            }
+        },
+        requirejs: {
+            account: {
+                options: {
+                    baseUrl: 'src/js',
+                    name: 'pages/account',
+                    out: '<%= compile_dir %>/src/js/account-<%= pkg.name %>-<%= pkg.version %>.js',
+                    optimize: 'none',
+                    mainConfigFile: "src/js/config/index.js"
+                }
+            }
+        }
+    };
+
+    grunt.initConfig(grunt.util._.extend(taskConfig, userConfig));
+
+    grunt.registerTask('require', [
+        'requirejs:development'
+    ]);
+
+    grunt.registerTask("development", [
+        'clean:build',
+
+        'copy:app_js',
+        'copy:app_assets',
+
+        'less:dev',
+
+        'copy:vendor_css',
+        'copy:vendor_js',
+        'copy:vendor_fonts'
+    ]);
+
+    grunt.registerTask('compile', [
+        'clean:compile',
+
+        'copy:app_assets_compile',
+
+        'copy:vendor_js_compile',
+
+        'requirejs:account',
+
+        'less:compile'
+    ]);
+
+    grunt.registerTask('debug', 'Main task for development', function () {
+        grunt.task.run('development');
+        grunt.task.run('watch');
+    });
+};
---
+++ build.config.js
@@ -0,0 +1,48 @@
+module.exports = {
+    build_dir: 'build',
+    compile_dir: 'bin',
+
+    app_files: {
+        js: [
+            'src/js/**/*.js'
+        ],
+        templates: [
+            'src/js/**/*.html',
+            'src/js/**/*.hbs'
+        ],
+        less: {
+            default: 'src/less/default.less'
+        }
+    },
+
+    vendor_files: {
+        js: [
+            'src/vendor/requirejs/require.js',
+            'src/vendor/requirejs/text.js',
+            'src/vendor/jquery/dist/jquery.js',
+            'src/vendor/underscore-1.8.3/underscore.js',
+            'src/vendor/bootstrap/dist/js/bootstrap.js',
+            'src/vendor/handlebars-0.11.2/handlebars.js',
+            'src/vendor/marionette/marionette.js',
+            'src/vendor/backbone/backbone.js',
+            'src/vendor/backbone/backbone_clear.js',
+            'src/vendor/backbone/backbone.babysitter.js',
+            'src/vendor/backbone/backbone.queryparam.js',
+            'src/vendor/backbone/backbone.routefilter.js',
+            'src/vendor/backbone/backbone.server.js',
+            'src/vendor/backbone/backbone.stickit.js',
+            'src/vendor/backbone/backbone.syphon.js',
+            'src/vendor/backbone/backbone.validation.js',
+            'src/vendor/backbone/backbone.wreqr.js'
+        ],
+        css: [],
+        fonts: [],
+        assets: []
+    },
+
+    compile_files: {
+        js: [
+            'src/vendor/requirejs/require.js'
+        ]
+    }
+};
